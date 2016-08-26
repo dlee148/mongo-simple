00aa5d88c368b943a9bab683e1461a8224ee7e61
Daniel Lee <dlee148@ucla.edu>
2016-08-26 10:14:59 -0700

    Added utility functions and realized this is going to be harder than i thought

--- Actions.cs
+++ Actions.cs
@@ -1,18 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
-using System.Threading.Tasks;
-using MongoDB.Bson;
+﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MongoSimple
 {
+    /// <summary>
+    /// Constitutes common CRUD actions.
+    /// </summary>
     public class Actions
     {
+        /// <summary>Database instance - object specific.</summary>
         protected static IMongoDatabase database;
+        /// <summary>Client instance - object specific.</summary>
         protected static IMongoClient client;
 
         /// <summary>
@@ -26,6 +28,8 @@ namespace MongoSimple
             database = client.GetDatabase(databaseName);
         }
 
+        // Read
+
         /// <summary>
         /// Fetches one document from the given collection matching the given key.
         /// </summary>
@@ -33,12 +37,58 @@ namespace MongoSimple
         /// <param name="collectionName">MongoDB collection name.</param>
         /// <param name="key">Field by which to identify document.</param>
         /// <param name="value">Value by which to identify document.</param>
-        /// <returns></returns>
+        /// <returns>One object of type T.</returns>
         public T FetchOne<T>(string collectionName, string key, string value) {
             var collection = database.GetCollection<BsonDocument>(collectionName);
             var filter = Builders<BsonDocument>.Filter.Eq(key, value);
             var result = collection.Find(filter).First();
             return BsonSerializer.Deserialize<T>(result);
+        }
+
+        /// <summary>
+        /// Fetches all documents in an array.
+        /// </summary>
+        /// <typeparam name="T">Data model for one document in the collection.</typeparam>
+        /// <param name="collectionName">MongoDB collection name.</param>
+        /// <returns>An array of objects of type T.</returns>
+        public T[] FetchAll<T>(string collectionName) where T : new()
+        {
+            var collection = database.GetCollection<BsonDocument>(collectionName);
+            var resultList = collection.Find(new BsonDocument()).ToList();
+
+            T[] deserializedResult = new T[resultList.Count];
+            for (int i = 0; i < resultList.Count; i++)
+            {
+                deserializedResult[i] = new T();
+                deserializedResult[i] = BsonSerializer.Deserialize<T>(resultList[i]);
+            }
+
+            return deserializedResult; 
+        }
+
+        // Utility
+
+        /// <summary>
+        /// Determines if the query string has >1 positional operators.
+        /// </summary>
+        /// <param name="query">Query string in question.<param>
+        /// <returns>Whether or not string has multiple pos. ops.</returns>
+        public bool HasMultiplePositionalOperators(string query)
+        {
+            return query.Count(x => x == '$') > 1;
+        }
+
+        public string ResolvePositionalOperators<T>(string collectionName, string query, Dictionary<string, string> ids)
+        {
+            var collection = database.GetCollection<BsonDocument>(collectionName);
+            var filter = Builders<BsonDocument>.Filter.Eq(ids.First().Key, ids.First().Value);
+            ids.Remove(ids.First().Key);
+
+            // There's going to be a lot of sketchy stuff that goes down here
+            // and it would be prudent to take some time to look at it
+            // http://stackoverflow.com/questions/2905187/accessing-object-property-as-string-and-setting-its-value
+
+            return "Help me.";
         }
     }
 }
